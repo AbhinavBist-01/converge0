3c6ab99dce0eba6dc1efc84646c492c76ef17790
Abhinav <abhinavbist05@gmail.com>
2026-07-08 01:32:04 +0530

    Set up Express server and add TypeScript support; include Zod and type definitions for Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello, TypeScript!");
+import express from "express";
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
